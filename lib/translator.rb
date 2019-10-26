a825836920d591affffb5682a4021472088737e5
Joseph  Louie <joseph161216@gmail.com>
2019-10-26 01:36:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,12 +6,12 @@ def load_library(path)
   #INPUT: HoA (1hash w/ array as val)
   hash = {}
   emoticons = YAML.load_file(path)
-  hash[:get_meaning] = {}
-  hash[:get_emoticon] = {}
+  hash["get_meaning"] = {}
+  hash["get_emoticon"] = {}
   
   emoticons.each do |meaning, arr|
     # binding.pry
-    hash[:get_meaning][meaning] = arr[1]
+    hash["get_meaning"][meaning] = arr[1]
   end
   # hash[:get_meaning][emoticons.keys[0]] = emoticons.keys[0][1]
   hash
